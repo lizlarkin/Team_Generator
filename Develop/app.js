80ae2dce67d97a89c5b73dc6810173d091aa4535
Liz <lizlarkin@highlandtechnology.com>
2020-12-11 11:22:27 -0800

    cleaned up app.js code to remove unhelpful comments and tests

--- Develop/app.js
+++ Develop/app.js
@@ -3,20 +3,13 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");                                        //VS CODE NOT AUTO FILLING ANYMORE
+const fs = require("fs");                                        
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
 // Employees Array
 const employees = []
@@ -86,7 +79,7 @@ const inquireManager = () => {
                 response.id, 
                 response.email, 
                 response.officeNumber);
-            employees.push(addManager);   // Is this pushing to global employees array???
+            employees.push(addManager);   
             addTeamMember();
 });
 }
@@ -123,7 +116,7 @@ const inquireEngineer = () => {
                 response.id, 
                 response.email, 
                 response.github);
-            employees.push(addEngineer);   // Is this pushing to global employees array???
+            employees.push(addEngineer);   
             addTeamMember();
 });
 }
@@ -160,25 +153,7 @@ const inquireIntern = () => {
             response.id, 
             response.email, 
             response.school);
-        employees.push(addIntern);   // Is this pushing to global employees array???
+        employees.push(addIntern);   
         addTeamMember();
 });
 }
-       
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
